bd2ddda4e4c47f3058abf9e0c92829de1075def7
Fiery-Paks <123paokin123@bk.ru>
2024-01-31 15:28:00 +0300

    First commit

---
+++ ConsoleAppFor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeExtraction rangeExtraction = new RangeExtraction();
+            rangeExtraction.Split("10-10,5454,65695,45,75");
+            return;
+            Console.Write("Введите число - ");
+            //Парсинг строки в число
+            int n = int.Parse(Console.ReadLine());
+            int summ = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                //Например - "0 + 1 = 1"
+                Console.WriteLine($"{summ} + {i} = {summ + i}");
+                summ = summ + i;
+            }
+            //Ожидание ввода символа с консоли чтобы консоль не зкрылась
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppFor/RangeExtraction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFor
+{
+    public class RangeExtraction
+    {
+        private string input;
+        private int[] mass;
+        private List<string> strings;
+
+        public RangeExtraction(string input)
+        {
+            this.input = input ?? throw new ArgumentNullException(nameof(input));
+        }
+
+        public RangeExtraction()
+        {
+        }
+
+        public void Split(string text)
+        {
+            strings = text.Split(',').ToList();
+        }
+
+        public int[] Parse()
+        {
+
+            return mass;
+        }
+    }
+}
